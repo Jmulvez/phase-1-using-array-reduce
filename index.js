236fbcb154fab3d0ef50b968f5cb2d558b2a1fc5
John <johnmulvey48@yahoo.com>
2021-12-23 09:28:04 -0500

    git@github.com:Jmulvez/phase-1-using-array-reduce.git

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Code your solution here
+let totalBatteries = batteryBatches;
+function totalBatteries(bateries) {
+    for (const batteries of batteryBatches) {
+        totalBatteries += batteries.batteryBatches;
+    };
+return totalBatteries;
+}
+console.log(totalBatteries(batteries));
